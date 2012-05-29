33eb5da031f043789708f2c6b0eb32b03f29a5f4
Myroslav Gavrylyak <efristical@gmail.com>
2012-05-29 14:32:11 +0300

    edit config.php

--- config.php
+++ config.php
@@ -1,7 +1,15 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Конфигурационный файл
  */
+
+$host = 'localhost';
+$pass = '';
+$user = 'root';
+$db = 'data';
+
+$b = mysql_connect($host, $user, $pass) or die(mysql_error());
+mysql_select_db($db, $b) or die(mysql_error());
+
+
 ?>
